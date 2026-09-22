5ffb397a8d662bf175ccf444fd1f5498d23c5946
stevembuvi <stevembuvi@gmail.com>
2026-09-22 19:15:44 +0300

    list_task

---
+++ list_task.py
@@ -0,0 +1,26 @@
+#create a new file list_task.py
+#trainees = ["John", [2, ["James","Mary"]]] --> This is a nested list(list within a list)
+
+
+trainees=["John",[2,["James","Mary"]]]
+print(len(trainees))
+#display 2 from the list
+print(trainees[1][0])
+
+#Output James  from the list
+print(trainees[1][1][0])
+
+#Using a method add 56 at the end of the list
+trainees.append('56')
+print(trainees)
+#Using a method add the name Mike between James and Mary
+trainees[1][1].insert(1, "Mike")
+print(trainees)
+#Change the value of 2 to 8
+trainees[1][0] = 8
+print(trainees)
+#Remove John and Mary from the list
+trainees.remove("John")
+print(trainees)
+trainees[0][1].remove('Mary')
+print(trainees)
